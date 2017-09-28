e0a321cc3a26290441891342ec46c5f37f0562b1
Mike Hoyt <derukimlh@gmail.com>
2017-09-28 05:41:41 -0700

    [update] install.sh [update] brew.list [add] npm.list [add] bin/scrub

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
 set -euf -o pipefail
 
@@ -6,23 +6,35 @@ set -euf -o pipefail
 
 # git clone https://github.com/mlhoyt/dotfiles.git
 
-# install "dotfiles"
+DOTFILES_ROOT=$( cd "$(dirname "$0")" ; pwd -P )
+
+# install "dot" files
 for i in .atom .bashrc .gitattributes .gitconfig .gnupg .vimrc bin
 do
-  ln -s ~/dotfiles/$i ~/$i
+  ln -s ${DOTFILES_ROOT}/$i ${HOME}/$i
 done
+
+# install git bash completion
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ${HOME}/.git-completion-bash
+echo "source ${HOME}/.git-completion-bash" >> ${HOME}/.bashrc
 
 # install brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # install brew packages
-for i in $(cat ~/dotfiles/brew.list)
+for i in $(cat ${DOTFILES_ROOT}/brew.list)
 do
   brew install ${i}
 done
 
+# install npm packages
+for i in $(cat ${DOTFILES_ROOT}/npm.list)
+do
+  npm install -g ${i}
+done
+
 # install atom packages
-apm install --packages-file ~/.atom/packages.list
+apm install --packages-file ${HOME}/.atom/packages.list
 
 # install JDK
 
