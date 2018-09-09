34a256af808e293ee7f503f15c175ce80de536c0
Mike Hoyt <derukimlh@gmail.com>
2018-09-09 07:45:29 -0700

    [update] install.sh

--- install.sh
+++ install.sh
@@ -20,38 +20,22 @@ ls -al ${HOME}/.vimrc
 
 # install "git"
 ln -sf ${DOTFILES_ROOT}/git/config ${HOME}/.gitconfig
+ln -sf ${DOTFILES_ROOT}/git/attributes ${HOME}/.gitattributes
+touch ${HOME}/.gitcredentials
 # install git bash completion
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ${HOME}/.git-completion-bash
 
-# install "atom"
-ln -sf ${DOTFILES_ROOT}/atom ${HOME}/.atom
-ls -al ${HOME}/.atom
-
-# # install "dot" files
-# for i in .gitattributes .gnupg bin
-# do
-#   ln -s ${DOTFILES_ROOT}/$i ${HOME}/$i
-# done
-# 
-# 
 # # install brew
 # /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-# 
+
 # # install brew packages
 # for i in $(cat ${DOTFILES_ROOT}/brew.list)
 # do
 #   brew install ${i}
 # done
-# 
+
 # # install npm packages
 # for i in $(cat ${DOTFILES_ROOT}/npm.list)
 # do
 #   npm install -g ${i}
 # done
-# 
-# # install atom packages
-# apm install --packages-file ${HOME}/.atom/packages.list
-# 
-# # install JDK
-# 
-# # install Google Chrome
