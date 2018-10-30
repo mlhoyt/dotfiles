c880c7727e1df089a9985912286b23b808bdc6f3
Mike Hoyt <derukimlh@gmail.com>
2018-10-30 05:32:53 -0700

    [update] install.sh - replaced bash with zsh

--- install.sh
+++ install.sh
@@ -1,40 +1,43 @@
 #!/usr/bin/env bash
-# This script is intended to be idempotent
+## This script is intended to be idempotent
 
 set -E -o errexit -o nounset -o pipefail
 trap "echo Error encountered -- exiting!" ERR
 
-# install XCode
-
-# git clone https://github.com/mlhoyt/dotfiles.git ${HOME}/dotfiles
+## install XCode (xcode-select --install)
 
 DOTFILES_ROOT=$( cd "$(dirname "$0")" ; pwd -P )
 
-# install "bashrc"
-ln -sf ${DOTFILES_ROOT}/bash/profile.bash ${HOME}/.profile
-ls -al ${HOME}/.profile
+## install "zsh"
+# chsh -s /bin/zsh
+## install "oh-my-zsh" (configuration manager)
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-# install "vimrc"
+## install "zshrc"
+ln -sf ${DOTFILES_ROOT}/zsh/zshrc ${HOME}/.zshrc
+ls -al ${HOME}/.zshrc
+
+## install "vimrc"
 printf "so ${DOTFILES_ROOT}/vim/vimrc.vim\n" > ${HOME}/.vimrc
 ls -al ${HOME}/.vimrc
 
-# install "git"
+## install "git"
 ln -sf ${DOTFILES_ROOT}/git/config ${HOME}/.gitconfig
 ln -sf ${DOTFILES_ROOT}/git/attributes ${HOME}/.gitattributes
 touch ${HOME}/.gitcredentials
-# install git bash completion
+## install git bash completion
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ${HOME}/.git-completion-bash
 
-# # install brew
+## install brew
 # /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# # install brew packages
+## install brew packages
 # for i in $(cat ${DOTFILES_ROOT}/brew.list)
 # do
 #   brew install ${i}
 # done
 
-# # install npm packages
+## install npm packages
 # for i in $(cat ${DOTFILES_ROOT}/npm.list)
 # do
 #   npm install -g ${i}
