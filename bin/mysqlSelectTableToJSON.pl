aa34d9664204289151eaca75c1385c25f2b4131b
Mike Hoyt <mhoyt@shujinko.io>
2019-07-13 06:14:13 -0700

    [add] bin/mysqlSelectTableToJSON.pl

---
+++ bin/mysqlSelectTableToJSON.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/env perl
+
+$foundColumns = 0;
+@COLUMNS = ();
+@DATA = ();
+
+# Parse input WSV MySQL SELECT syntax
+while(<>) {
+  next if( /^\s*$/ || /^\+\-+/ );
+
+  # Strip leading / trailing table boundary
+  s/^\s*\|\s*//;
+  s/\s*\|\s*$//;
+
+  if ( !$foundColumns ) {
+    @COLUMNS = split( /\s*\|\s*/ );
+    $foundColumns = 1;
+  }
+  else {
+    @fields = split( /\s*\|\s*/ );
+
+    my @json_items = ();
+    for ( my $i=0; $i < $#COLUMNS; $i++ ) {
+      push @json_items, sprintf("\"%s\": \"%s\"", $COLUMNS[$i], $fields[$i]);
+    }
+
+    push @DATA, sprintf("{%s}", join(", ", @json_items));
+  }
+}
+
+# Output JSON syntax
+printf("[%s]\n", join(", ", @DATA));
